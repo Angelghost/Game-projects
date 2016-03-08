df29a04de61ae3dffd8837c9991e7e878f1c17f6
lduchesne <lduchesne@ITEM-B24014.dhcp.ancy.fr.sopra>
2016-03-08 10:02:26 +0100

    Add some js

--- game-lib/game-lib-web/src/main/webapp/resources/js/game-lib.js
+++ game-lib/game-lib-web/src/main/webapp/resources/js/game-lib.js
@@ -1,31 +1,69 @@
-var oldComponent;
+var oldFirstComponent;
 
-var oldTile;
+var oldSecondComponent;
+
+var oldFirstTile;
+
+var oldSecondTile;
+
+var isCallByUpdate;
 
 function selectTile(component, tile) {
 
-	if (oldComponent != null) {
-		oldComponent.style.border = "none";
+	if (!isCallByUpdate) {
+		if (oldFirstComponent != null && oldSecondComponent != null) {
+			oldFirstComponent.style.border = "none";
+			oldSecondComponent.style.border = "none";
+		}
+
+		if (oldFirstComponent == null
+				|| (oldFirstComponent != null && oldSecondComponent != null)) {
+			oldFirstComponent = component;
+			oldFirstTile = tile;
+			oldSecondComponent = null;
+			oldSecondTile = null;
+		} else if (oldFirstComponent != null && oldSecondComponent == null) {
+			oldSecondComponent = component;
+			oldSecondTile = tile;
+		}
+
 	}
+
 	component.style.border = "rgb(255, 0, 0) solid 1px";
-	oldComponent = component;
-	oldTile = tile;
+	if (oldSecondComponent != null) {
+		oldSecondComponent.style.border = "rgb(255, 0, 0) solid 1px";
+	}
+
 	// dsiplay menu
 	$("#tileMenu").empty();
 	$("#tileMenu").append(
-			"<p>Tile selected  (" + tile.x + "," + tile.y + ")</p>");
-	if (("humanoideList" in tile)) {
+			"<p>Tile selected  (" + oldFirstTile.x + "," + oldFirstTile.y
+					+ ")</p>");
+	if (("humanoideList" in oldFirstTile)) {
 		$("#tileMenu").append(
-				"<p>Human : " + tile.humanoideList.length + "</p>");
+				"<p>Human : " + oldFirstTile.humanoideList.length + "</p>");
 	}
+	if (oldSecondTile != null) {
+		$("#tileMenu").append(
+				"<p>Second Tile selected  (" + oldSecondTile.x + ","
+						+ oldSecondTile.y + ")</p>");
 
+		if (("humanoideList" in oldSecondTile)) {
+			$("#tileMenu")
+					.append(
+							"<p>Human : " + oldSecondTile.humanoideList.length
+									+ "</p>");
+		}
+	}
+	isCallByUpdate = false;
 };
 
 function updateSelectedTile() {
-	if (oldTile != null) {
-		var elem = document.getElementById("tile-" + oldTile.x + "-"
-				+ oldTile.y);
+	if (oldFirstTile != null) {
+		var elem = document.getElementById("tile-" + oldFirstTile.x + "-"
+				+ oldFirstTile.y);
 		if (typeof elem.onclick == "function") {
+			isCallByUpdate = true;
 			elem.onclick.apply(elem);
 		}
 	}
